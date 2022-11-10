59ab861837fe52a5933d4bb8ab6941a5de10a485
julia-kheil <115854674+julia-kheil@users.noreply.github.com>
2022-11-10 20:10:30 +0700

    Create jawaban.py

---
+++ jawaban.py
@@ -0,0 +1,23 @@
+String = "algoritma dan pemrograman"
+print(String)
+print(20*"-")
+
+Integer = 300703
+print(Integer)
+print(20*"-")
+
+Float = 3007.03
+print(Float)
+print(20*"-")
+
+List = ["merah","putih","item"]
+print(List)
+print(20*"-")
+
+Boolean1 = True
+print(Boolean1)
+print(20*"-")
+
+Boolean2 = False
+print(Boolean2)
+print(20*"-")
