061dfd27cf9a1fcea9e20f37dde61406416c1645
julia-kheil <115854674+julia-kheil@users.noreply.github.com>
2022-11-10 20:09:41 +0700

    Create jawaban.java

---
+++ jawaban.java
@@ -0,0 +1,30 @@
+public class Jawaban {
+public static void main (String [] args){
+byte value_byte = 10;
+short value_short = 300;
+int value_int = 20000;
+long value_long = 99990000000L;
+System.out.println("Nilai tipe data byte = " + value_byte);
+System.out.println("Nilai tipe data short = " + value_short);
+System.out.println("Nilai tipe data int = " + value_int);
+System.out.println("Nilai tipe data long = " + value_long);
+
+float short_pi = 30.4f;
+double long_pi = 3.210495342322444443d;
+System.out.println("Nilai short pi = " + short_pi);
+System.out.println("Nilai tipe long pi = " + long_pi);
+
+char huruf_A = 'A', huruf_A_juga = 60;
+char huruf_B = 'B', huruf_B_juga = 40;
+char persen = '%', persen_juga = 37;
+char tandakutip = '\'';
+System.out.println(huruf_A+" dan "+huruf_A_juga);
+System.out.println(huruf_B+" dan "+huruf_B_juga);
+System.out.println(persen+" dan "+persen_juga);
+System.out.println(tandakutip);
+
+String hello = "julia-algoritma dan pemograman!!!";
+System.out.println(hello);
+
+}
+}
